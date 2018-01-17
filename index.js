434ef160599436ff767ada7277c30b2538915788
Lucy Ormiston <lucyormiston@gmail.com>
2018-01-17 08:49:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ function logWhisper(string) {
 }
 
 
-+function sayHiToGrandma(string) {
+function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }
   else if (string === "I love you, Grandma.") {
-    return "I love you, too."
- +  }
- +  else {
- +    return ("I can't hear you!")
- +}
- +}
+  return "I love you, too."
+  }
+  else {
+    return ("I can't hear you!")
+ }
+}
